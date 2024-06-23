dfefb42a5c232d0a1e5f92e4f00e17f4e9b3f13b
sunwchoi <chltjsdn103@naver.com>
2024-06-24 08:22:49 +0900

    Feat: Tank Movement
    1. make Tank Vehicle
    2. make Turret and Barrel on Vehicle
    3. add Movement and Rotation

---
+++ Source/WorldOfTTank/Public/CSW/Tank.h
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Tank.generated.h"
+
+UCLASS()
+class WORLDOFTTANK_API ATank : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATank();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components", meta=(AllowPrivateAccess="true"))
+	class UCapsuleComponent* CapsuleComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* TurretMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* BarrelMesh;
+
+	UPROPERTY(VisibleAnywhere, Category="Componets")
+	class USpringArmComponent* SpringArmComp;
+
+	UPROPERTY(VisibleAnywhere, Category="Components")
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float Speed = 300;
+	
+	UPROPERTY(EditAnywhere, Category="Movement")
+	float TurnRate = 50;
+	
+	void	Move(float Value);
+	void	Turn(float Value);
+	void	LookRightLeft(float Value);
+	
+
+	APlayerController* PlayerControllerRef;
+protected:
+	void	RotateTurretAndBarrel(FVector TargetLocation);
+};
